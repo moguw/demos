147f8331859bcb68be2229593b34532e73fae7c7
edison wang <876793060@qq.com>
2023-11-17 16:46:40 +0800

    Update run.py

--- Pytest_Demo/Yugo/run.py
+++ Pytest_Demo/Yugo/run.py
@@ -4,7 +4,7 @@ from common import allure_control
 
 if __name__ == "__main__":
     markers = os.environ['markers']
-    pytest.main(["-m %s"] % markers)
+    pytest.main(["-m %s"]) % str(markers)
     # pytest.main(["-m smoke"])
     # pytest.main(["-m regression"])
 
